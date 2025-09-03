1f6424cedbd67ee949485dee6b40a0830601643d
Mansha Dhawan <mansha@Manshas-MacBook-Air.local>
2025-09-03 23:51:23 +0530

    Add solution for sum of two numbers

---
+++ 100days.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int a,b,c;
+    printf("enter first no");
+    scanf("%d",&a);
+    printf("enter second no");
+    scanf("%d",&b);
+    c=a+b;
+    printf("sum is %d\n",c);
+    return 0;
+
+}
